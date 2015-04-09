bc85fdddd3735eb8865da81dbfc0c39e712a921a
TheLastSoulTiger <whitedragon3907@gmail.com>
2015-04-09 18:23:09 +0200

    Test

---
+++ Random1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class SpaceName
+{
+    public SpaceName()
+	{
+
+        string[] maleNames = { "Rufus", "Bear", "Dakota", "Fido", 
+                                "Vanya", "Samuel", "Koani", "Volodya", 
+                                "Prince", "Yiska" };
+        string[] femaleNames = { "Maggie", "Penny", "Saya", "Princess", 
+                                  "Abby", "Laila", "Sadie", "Olivia", 
+                                  "Starlight", "Talla" };
+
+	}
+}
